0024260a0f907c01077a9f0717d994aa6d61cdbd
hardcoder <hardcode.dec@gmai.com>
2022-07-17 23:22:07 +0300

    Add font, some colors

--- app/src/main/java/hardcoder/dev/voicer/ui/theme/Type.kt
+++ app/src/main/java/hardcoder/dev/voicer/ui/theme/Type.kt
@@ -2,27 +2,45 @@ package hardcoder.dev.voicer.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import hardcoder.dev.voicer.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+    listOf(
+        Font(R.font.ubuntu_bold, FontWeight.Bold),
+        Font(R.font.ubuntu_medium, FontWeight.Medium),
+        Font(R.font.ubuntu_regular, FontWeight.Normal)
+    )
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Thin,
+        fontSize = 16.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = fonts,
+        fontStyle = FontStyle.Italic,
+        fontSize = 16.sp
     )
-    */
 )
